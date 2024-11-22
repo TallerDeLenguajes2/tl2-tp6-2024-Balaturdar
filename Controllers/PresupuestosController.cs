a42ef3b199b3c4112e7890c9cbe9df96db9acdee
VillafañeNahuel <151423052+Balaturdar@users.noreply.github.com.>
2024-11-22 01:28:22 -0300

    avance en controlador y vistas presupuestos

--- Controllers/PresupuestosController.cs
+++ Controllers/PresupuestosController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 
 public class PresupuestosController : Controller
@@ -17,37 +18,89 @@ public class PresupuestosController : Controller
     }
     //En el controlador de Presupuestos: Listar, Crear, Modificar y Eliminar Presupuestos.
 
-    public IActionResult AltaPresupuesto(){
+    public IActionResult DetallesDelPresupuesto(int id)
+    {
+        return View(repoPresupuestos.ObtenerPresupuestoId(id));
+    }
+
+    public IActionResult AltaPresupuesto()
+    {
         return View();
     }
-    public IActionResult CrearPresupuesto(Presupuestos presupuesto){
+
+    [HttpPost]
+    public IActionResult CrearPresupuesto(Presupuestos presupuesto)
+    {
         repoPresupuestos.CrearNuevoPresupuesto(presupuesto);
         return RedirectToAction("Index");
     }
 
-    public IActionResult AgregarProducto(int id){
-        //repoproductos
+
+    [HttpGet]
+
+    public IActionResult AgregarProductoAPresupuesto(int id)
+    {
+        ProductosRepository repoProductos = new ProductosRepository();
+        List<Productos> productos = repoProductos.ListarProductos();
+        ViewData["Productos"] = productos.Select(p => new SelectListItem
+        {
+            Value = p.IdProductos.ToString(),
+            Text = p.Descripcion
+        }).ToList();
+
         return View(id);
     }
 
-    [HttpGet]
-    public IActionResult ModificarPresupuesto(int id)
+    [HttpPost]
+    public IActionResult AgregarProductoEnPresupuesto(int idPresupuesto, int idProducto, int cantidad)
     {
-        var producto  = repoPresupuestos.ObtenerPresupuestoId(id);
+        repoPresupuestos.AgregarProducto(idPresupuesto, idProducto, cantidad);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+
+    public IActionResult EliminarProductoAPresupuesto(int id)
+    {
+        Presupuestos presupuesto = repoPresupuestos.ObtenerPresupuestoId(id);
+        ViewData["Productos"] = presupuesto.Detalle.Select(p => new SelectListItem
+        {
+            Value = p.Producto.IdProductos.ToString(),
+            Text = p.Producto.Descripcion
+        }).ToList();
+
+        return View(id);
+    }
+
+    [HttpPost]
+    public IActionResult EliminarProductoEnPresupuesto(int idPresupuesto, int idProducto)
+    {
+        repoPresupuestos.EliminarProducto(idPresupuesto, idProducto);
+        return RedirectToAction("Index");
+    }
+
+
+    [HttpGet]
+    public IActionResult ModificarPresupuestos(int id)
+    {
+        var producto = repoPresupuestos.ObtenerPresupuestoId(id);
         return View(producto);
     }
-    public IActionResult ModificarPresupuesto(Presupuestos presupuesto){
+    public IActionResult ModificarPresupuestos(Presupuestos presupuesto)
+    {
         repoPresupuestos.ModificarPresupuesto(presupuesto);
         return RedirectToAction("Index");
     }
 
-    
-    public IActionResult EliminarPresupuesto(int id){
+
+    public IActionResult EliminarPresupuesto(int id)
+    {
         return View(repoPresupuestos.ObtenerPresupuestoId(id));
     }
-    public IActionResult EliminarPresupuestoid(int id){
+    public IActionResult EliminarPresupuestoid(int id)
+    {
         repoPresupuestos.EliminarPresupuestoPorId(id);
-        return RedirectToAction ("Index");
+        return RedirectToAction("Index");
     }
-    
+
 }
--- Models/Presupuestos.cs
+++ Models/Presupuestos.cs
@@ -1,13 +1,13 @@
 public class Presupuestos{
     private int idPresupuesto;
     private string nombreDestinatario;
-    private DateTime fechaCrecion;
+    private DateTime fechaCreacion;
     private List<PresupuestoDetalle> detalle;
 
     public int IdPresupuesto { get => idPresupuesto; set => idPresupuesto = value; }
     public string NombreDestinatario { get => nombreDestinatario; set => nombreDestinatario = value; }
     public List<PresupuestoDetalle> Detalle { get => detalle; set => detalle = value; }
-    public DateTime FechaCrecion { get => fechaCrecion; set => fechaCrecion = value; }
+    public DateTime FechaCrecion { get => fechaCreacion; set => fechaCreacion = value; }
 
     public double MontoPresupuesto(){
         
